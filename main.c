6925574f84a0321a74b18eb13044b40f2108f92f
Brand C <iron@localhost.localdomain>
2016-12-16 00:32:10 -0800

    rearranged source

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 #include <netdb.h>
 #include <sys/socket.h>
 
+/*ipv4 string stuff*/
+#include "utils.c"
+
 /*The resulting hostname will be returned in host and the length in hostlen*/
 bool ipv4_to_hostname(const char* ip, char* host, size_t *hostlen)
 {
@@ -66,19 +69,6 @@ bool ipv4_to_hostname(const char* ip, char* host, size_t *hostlen)
 		return ret;
 }
 
-void uint32_to_ipv4(char *dest, size_t n, uint32_t ip)
-{
-	/*IPV4 integer to string*/
-	snprintf(dest,
-		 n,
-		 "%u.%u.%u.%u",
-		 (ip >> 24) & 0xFF,
-		 (ip >> 16) & 0xFF,
-		 (ip >>  8) & 0xFF,
-		 (ip >>  0) & 0xFF
-	);
-}
-
 void rdns_range(uint32_t start, uint32_t end)
 {
 	uint32_t ipnum;
@@ -115,71 +105,6 @@ void rdns_range(uint32_t start, uint32_t end)
 	} while (true);
 }
 
-#include <unistd.h>
-
-uint32_t strn_to_uint32(char *num, size_t *n)
-{
-	char c;
-	size_t i;
-	uint32_t ret;
-
-	ret = 0;
-	for (i = 0; i < (*n); i++) {
-		c = num[i];
-
-		if (c < '0' || c > '9')
-			break;
-		ret *= 10;
-		ret += (c - '0');
-	}
-
-	*n = i;
-	return ret;
-}
-
-bool ipv4_to_uint32(char* ip, size_t n, uint32_t* result)
-{
-	size_t i;
-	size_t len;
-	size_t octets;
-	uint32_t ret, raw_value;
-
-	/*shortest ipv4 address: 0.0.0.0 which is 7 bytes in length*/
-	/*longest ipv4 address: 255.255.255.255 which is 15 bytes in length*/
-	if (n < 7 || n > 15)
-		return false;
-
-	ret    = 0;
-	octets = 0;
-	for (i = 0; i < n; i++) {
-		ret <<= 8;
-		/*Since 256 is the longest possible number for an ipv4 address*/
-		len = 3;
-		raw_value = strn_to_uint32(ip+i, &len);
-		/*no octet value may be greater than 255*/
-		if (raw_value > 255)
-			return false;
-		/*enforces length requirements of ipv4 octets*/
-		else if (len > 3 || !len)
-			return false;
-
-		i   += len;
-		ret += raw_value;
-
-		/*enforces the ipv4 dots*/
-		if (((i+1) < len) && (ip[i+1] != '.'))
-			return false;
-		octets++;
-	}
-
-	/*an ipv4 address must be exactly four octets in length*/
-	if (octets != 4)
-		return false;
-
-	*result = ret;
-	return true;
-}
-
 void usage(void)
 {
 	fprintf(stderr, "-h prints this text\n");
@@ -187,6 +112,7 @@ void usage(void)
 	exit(1);
 }
 
+#include <unistd.h>
 int main(int argc, char *argv[])
 {
 	int opt;
